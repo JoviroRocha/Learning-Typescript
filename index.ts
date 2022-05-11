b1dfe5108f4ca866230f226007d1b598f9b1c54f
JoviroRocha <joviro_neto@hotmail.com>
2022-05-11 15:52:53 -0300

    Begin

---
+++ index.ts
@@ -0,0 +1,17 @@
+let displayName : string = "Pedro";
+let displayLastName = "Souza" as string;
+let displayAge : number = 32;
+let displayCode = 133332908 as number;
+
+// Describe a object 
+interface Person {
+    displayName : string;
+    displayLastName :string;
+    displayAge : number;
+    displayCode : number; 
+}
+
+function getPerson(trackingNumber : string) : Person {
+
+    return null;
+}
